09ff3c9e5fb336506c032d992b0d157abc156cc4
WtrSt9704 <wtrst9704@gmail.com>
2020-11-17 18:16:16 +0900

    jeon and song's update

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { User } = require("../models/User");
 
 const { auth } = require("../middleware/auth");
+const {Payment} = require('../models/Payment');
+const payment = require('../models/Payment');
+
+const async = require('async');
+const Product = require('../models/Product');
 
 //=================================
 //             User
@@ -59,7 +64,7 @@ router.post("/login", (req, res) => {
     });
 });
 
-router.get("/logout", auth, (req, res) => {
+router.post("/logout", auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
@@ -67,5 +72,91 @@ router.get("/logout", auth, (req, res) => {
         });
     });
 });
+
+
+successBuy
+
+router.get('/successBuy',auth,(req,res) =>{
+
+    let history = [];
+    let transactionData= {};
+    //1. put a bref payment information inside user collection
+
+    req.body.cartDetail.forEach((item) =>{
+        history.push({
+            dateOfPurchase : Date.now(),
+            name : item.title,
+            id : item.id,
+            price : item.price,
+            quantity : item.quantity,
+            paymentId: req.body.paymentData.paymentId
+        })
+    })
+
+    //2.  put payment information that come from Paypal into Payment Collection
+
+    transactionData.user = {
+
+        id : req.user._id,
+        name : req.user.name,
+        lastname : req.user.lastname,
+        email : req.user.email
+
+
+    }
+
+
+    transactionData.data = req.body.paymentData;
+    transactionData.product = history
+
+    User.findOneAndUpdate(
+
+        {_id : req.user._id},
+        {$push : {history : history},$set : {cart : [] } },
+        {new : true},
+        (err, user)=>{
+            if(err) return res.json({success : false, err});
+
+           const  Payment = new Payment(transactionData)
+           if(err) return res.json({success: false, err});
+
+            //3. increase the amount of number for the sold information
+
+            //first we need to know how many product were sold in this transation for each of product
+
+
+            let products  = [];
+            doc.products.forEach(item => {
+                products.push({id : item.id,quantity : item.quantity})
+            })
+            async.eachSeries(products, (item,callback) =>{
+                Product.update(
+                    {id : item.id},
+                    {
+                        $inc : {
+                            "sold":item.quantity
+                        }
+                    },
+                    {new : false},
+                    callback
+                )
+            }, (err) => {
+                if(err) return res.json({success: false,err})
+                res.status(200).json({
+                    success : true,
+                    cart : user.cart,
+                    cartDetail : []
+                })
+            })
+        }
+
+    )
+
+
+   
+
+
+}) 
+
 
 module.exports = router;
