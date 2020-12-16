917a73455341c1ae12efcc01a37495230f620029
WtrSt9704 <wtrst9704@gmail.com>
2020-12-16 15:10:25 +0900

    icon modify

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -20,30 +20,22 @@ import { EllipsisOutlined } from '@ant-design/icons';
 function LandingPage(props) {
     const history = useHistory();
 
+const icons = {border: "white", //1px solid #364d79
+color: "#364d79",
+background: "white",
+width : '150px',
+margin:'45px',
+fontSize: '32px',
+ 
+};
+
     const buttonSt = {
         border: "white", //1px solid #364d79
         color: "#364d79",
         background: "white",
         width : '150px',
-        margin:'3%',
+        margin:'45px',
     };
-    const buttonSt2 = {
-      border: "white", //1px solid #364d79
-      color: "#364d79",
-      background: "white",
-      width : '150px',
-      margin:'3%',
-      marginleft : '3%',
-  };
-
-  const buttonSt3 = {
-    border: "white", //1px solid #364d79
-    color: "#364d79",
-    background: "white",
-    width : '150px',
-    margin:'3%',
-    marginright : '3%',
-};
 
     useEffect(() => {
         axios.post('api/product/products')
@@ -85,21 +77,30 @@ function LandingPage(props) {
             </div>
 
         </Carousel>
-        <section>
+         
+
         <div style={{ fontWeight: 'bold', display: 'flex', justifyContent: 'left', marginTop: "2%"}}>
-        <h1> &nbsp;&nbsp;&nbsp;&nbsp; Category</h1>
+        <h1> &nbsp;&nbsp;&nbsp;&nbsp;Category</h1>
         </div>
         <div style={{ fontWeight: 'bold', display: 'flex', justifyContent: 'left'}}>
-        <label style={{ color: "#8d8d8d"}}>&nbsp;&nbsp;&nbsp;&nbsp;Choose the Exhibition category of your desire! {' '}  </label> 
+        <label style={{ color: "#8d8d8d"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Choose the Exhibition category of your desire! {' '}  </label> 
         </div>
-        </section>
+<div  style={{display: 'flex', justifyContent: 'center'}} >
+
+        <IdcardOutlined type="primary" style = {icons} onClick={() => categoryButtonHandler(1)}><IdcardOutlined style={{color: "#364d79" }} /> BUSINESS</IdcardOutlined>  
+        <DesktopOutlined type="primary" style = {icons} onClick={() => categoryButtonHandler(2)}><DesktopOutlined style={{color: "#364d79" }} />IT</DesktopOutlined>    
+        <CarOutlined type="primary" style = {icons} onClick={() => categoryButtonHandler(3)}><CarOutlined style={{color: "#364d79" }} />AUTOMOBILE</CarOutlined>    
+        <PlaySquareOutlined type="primary" style = {icons} onClick={() => categoryButtonHandler(4)}><PlaySquareOutlined style={{color: "#364d79" }} />MEDIA</PlaySquareOutlined>     
+        <EllipsisOutlined type="primary" style = {icons} onClick={() => categoryButtonHandler()}><EllipsisOutlined style={{color: "#364d79" }} />OTHERS</EllipsisOutlined>
+
+        </div>
         <div  style={{display: 'flex', justifyContent: 'center'}} >
 
-        <Button type="primary" style = {buttonSt2} onClick={() => categoryButtonHandler(1)}><IdcardOutlined style={{color: "#364d79" }} /> BUSINESS</Button>  
+        <Button type="primary" style = {buttonSt} onClick={() => categoryButtonHandler(1)}><IdcardOutlined style={{color: "#364d79" }} /> BUSINESS</Button>  
         <Button type="primary" style = {buttonSt} onClick={() => categoryButtonHandler(2)}><DesktopOutlined style={{color: "#364d79" }} />IT</Button>    
         <Button type="primary" style = {buttonSt} onClick={() => categoryButtonHandler(3)}><CarOutlined style={{color: "#364d79" }} />AUTOMOBILE</Button>    
         <Button type="primary" style = {buttonSt} onClick={() => categoryButtonHandler(4)}><PlaySquareOutlined style={{color: "#364d79" }} />MEDIA</Button>     
-        <Button type="primary" style = {buttonSt3} onClick={() => categoryButtonHandler()}><EllipsisOutlined style={{color: "#364d79" }} />OTHERS</Button>
+        <Button type="primary" style = {buttonSt} onClick={() => categoryButtonHandler()}><EllipsisOutlined style={{color: "#364d79" }} />OTHERS</Button>
 
         </div>
       </div>  
