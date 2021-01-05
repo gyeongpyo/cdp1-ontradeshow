253fe7fd350f8f325523edc1242b73a0ca7bb87c
WtrSt9704 <wtrst9704@gmail.com>
2021-01-05 10:28:25 +0900

    21.01.04 Jeon

--- server/routes/product.js
+++ server/routes/product.js
@@ -85,6 +85,57 @@ router.post("/products", (req, res) => {
   }
 });
 
+
+router.post("/products2", (req, res) => {
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+  let term = req.body.searchTerm;
+
+  let findArgs = {};
+
+  for (let key in req.body.filters) {
+    if (req.body.filters[key].length > 0) {
+      if (key === 'price') {
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1]
+        }
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  console.log(findArgs);
+  console.log('term: ', term)
+  if (term) {
+    Product.find(findArgs)
+    .find({ $text: { $search: term }})
+    .populate("writer")
+    .skip(skip)
+    .limit(limit)
+    .exec((err, info) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res.status(200).json({ 
+        success: true, info ,
+        postSize: info.length
+      });
+    })
+  } else {
+    Product.find(findArgs)
+    .populate("writer")
+    .skip(skip)
+    .limit(limit)
+    .exec((err, info) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res.status(200).json({ 
+        success: true, info ,
+        postSize: info.length
+      });
+    })
+  }
+});
+
 router.post("/getProducts",auth,(req,res)=> {
 
   Product.find()
